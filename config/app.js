391c188f99fbf00f9003d1c10df720a972ed9e22
Oli Forster <oliforster798@gmail.com>
2016-04-03 15:38:57 +0100

    can sign in with facebook

--- config/app.js
+++ config/app.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   databaseUrl: 'mongodb://localhost/projectFour',
   secret: 'hfjkasfjsaefj',
-  appUrl: 'http://localhost:8000'
+  appUrl: 'http://localhost:3000'
 }
