c388f8b199c9e1456818036841aba49625ab71f7
Oli Forster <oliforster798@gmail.com>
2016-04-04 19:48:55 +0100

    fixed show page

--- public/js/controllers/user.js
+++ public/js/controllers/user.js
@@ -1,17 +1,18 @@
 angular.module('testApp')
   .controller('UserController', UserController);
 
-  UserController.$inject = ['User', '$state'];
-  function UserController(User, $state) {
-    var self = this;
-    self.searchHistory = [];
+UserController.$inject = ['User', '$state', 'tokenService'];
+function UserController(User, $state, tokenService) {
+  var self = this;
+  self.searchHistory = [];
+  this.currentUser = tokenService.getUser();
 
-    this.userPage = function () {
-      User.get({id: self.currentUser._id}).$promise.then(function (user) {
-        self.searchHistory = user.searchHistory;
-      });
-      $state.go('user')
-    }
-
-
+  this.userPage = function () {
+    User.get({id: self.currentUser._id}).$promise.then(function (user) {
+      self.searchHistory = user.searchHistory;
+    });
+    $state.go('user')
   }
+
+
+}
