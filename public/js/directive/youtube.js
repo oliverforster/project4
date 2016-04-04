b42f6d68bc76d0b3b538cb295673755cd20ef8fb
Oli Forster <oliforster798@gmail.com>
2016-04-04 12:53:54 +0100

    trailer showing

--- public/js/directive/youtube.js
+++ public/js/directive/youtube.js
@@ -8,7 +8,7 @@ function Youtube($sce) {
       restrict: 'EA',
       scope: { code:'=' },
       replace: true,
-      template: '<div style="height:400px;"><iframe style="overflow:hidden;height:100%;width:100%" width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>',
+      template: '<iframe width="560" height="315" src="{{url}}" frameborder="0" allowfullscreen></iframe>',
       link: function (scope) {
           scope.$watch('code', function (newVal) {
              if (newVal) {
