f5ce673f843941da9636dbce08c54340b7c2f851
Oli Forster <oliforster798@gmail.com>
2016-04-04 14:16:27 +0100

    nice close icon

--- public/js/directive/youtube.js
+++ public/js/directive/youtube.js
@@ -18,19 +18,3 @@ function Youtube($sce) {
       }
     };
   }
-
-
-  // controller: function(scope) {
-  //   scope.openFancybox = function (url) {
-  //     $http.get(url).then(function(response) {
-  //       if (response.status == 200) {
-  //
-  //         var template = angular.element(response.data);
-  //         var compiledTemplate = $compile(template);
-  //         compiledTemplate($scope);
-  //
-  //         $.fancybox.open({ content: template, type: 'html' });
-  //         }
-  //       });
-  //     }
-  //   }
