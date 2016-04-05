2446f1fbf77330849f47b08338cb8ca34872b4c9
Oli Forster <oliforster798@gmail.com>
2016-04-05 10:46:42 +0100

    made request form

--- public/js/controllers/food.js
+++ public/js/controllers/food.js
@@ -3,5 +3,21 @@ angular.module('testApp')
 
 FoodController.$inject = ['$http'];
 function FoodController($http) {
-  
+  var self = this;
+  this.search = {};
+
+  self.newRequest = function () {
+    $http({
+      method: "POST",
+      url: "http://localhost:3000/api/food",
+      data: {request: this.search, user: this.currentUser}
+    })
+    .then(function(response) {
+      console.log("ping!!");
+      })
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  }
 }
