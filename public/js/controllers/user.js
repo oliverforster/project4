ebe68c697d6b804bf5cf817bfb7669f823dcc777
Oli Forster <oliforster798@gmail.com>
2016-04-06 10:09:05 +0100

    saving search history

--- public/js/controllers/user.js
+++ public/js/controllers/user.js
@@ -4,12 +4,14 @@ angular.module('testApp')
 UserController.$inject = ['User', '$state', 'tokenService'];
 function UserController(User, $state, tokenService) {
   var self = this;
-  self.searchHistory = [];
-  this.currentUser = tokenService.getUser();
+  self.tvHistory = [];
+  self.foodHistory   = [];
 
   this.userPage = function () {
+    this.currentUser = tokenService.getUser();
     User.get({id: self.currentUser._id}).$promise.then(function (user) {
-      self.searchHistory = user.searchHistory;
+      self.tvHistory = user.tvHistory;
+      self.foodHistory = user.foodHistory;
     });
     $state.go('user')
   }
