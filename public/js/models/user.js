45bf33a5fd65bc10d0a10c663433e9b737e2525f
Oli Forster <oliforster798@gmail.com>
2016-04-07 18:20:56 +0100

    removed some more local hosts

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,9 +1,9 @@
 angular.module('testApp')
   .factory('User', User);
 
-User.$inject = ['$resource'];
-function User($resource) {
-  return $resource("http://localhost:3000" + '/users/:id', {id: '@_id'}, {
+User.$inject = ['$resource', 'API_URL'];
+function User($resource, API_URL) {
+  return $resource(API_URL + '/users/:id', {id: '@_id'}, {
     update: { method: "PUT"}
   });
 }
