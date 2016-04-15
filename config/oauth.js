9fd2aa008c3a062aca97466b3f3d89290bb02ec8
Oli Forster <oliforster798@gmail.com>
2016-04-15 13:06:46 +0100

    2.5 again

--- config/oauth.js
+++ config/oauth.js
@@ -1,6 +1,6 @@
 module.exports = {
   facebook: {
-    accessTokenUrl: 'https://graph.facebook.com/v2.6/oauth/access_token',
-    profileUrl: 'https://graph.facebook.com/v2.6/me?fields=id,email,name,picture.width(200).height(200)'
+    accessTokenUrl: 'https://graph.facebook.com/v2.5/oauth/access_token',
+    profileUrl: 'https://graph.facebook.com/v2.5/me?fields=id,email,name,picture.width(200).height(200)'
   }
 };
